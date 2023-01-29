a943634eee32cb84ed31d34c56efa8e84102842c
devopsjyotir <jyotirjy@gmail.com>
2023-01-28 21:44:25 -0500

    update package.json

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -71,11 +71,10 @@ const Hero = () => {
   );
   const five = (
     <a
-      className="email-link"
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      {/* Check out my course! */}
     </a>
   );
 
